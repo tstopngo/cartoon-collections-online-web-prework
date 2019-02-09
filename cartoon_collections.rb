c63916bc3b0abfb5d76537472b61c619eabeb08d
Tracy Ngo <tngo1206@gmail.com>
2019-02-09 22:55:25 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,8 +4,8 @@ def roll_call_dwarves(dwarves_array)
   end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_array) # code an argument here
+  planeteer_array.collect {|element| element.upcase}
 end
 
 def long_planeteer_calls# code an argument here
