e7b8ad92ed8d7ee0bdcd071e597af01db9f7789a
Tracy Ngo <tngo1206@gmail.com>
2019-02-09 23:12:45 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,7 +14,8 @@ def long_planeteer_calls(calls_array)  # code an argument here
   end
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(string_array) #code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  
 end
