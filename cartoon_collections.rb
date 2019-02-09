00911553dff19eddff8550ed44b6c6b5492d68c2
Tracy Ngo <tngo1206@gmail.com>
2019-02-09 23:21:45 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,5 +17,8 @@ end
 def find_the_cheese(string_array) #code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  if string_array.include("chedder") ||
+  if string_array.include("chedder") || string_array.include("gouda") || string_array.include("camembert")
+    return true
+  else
+    return false
 end
