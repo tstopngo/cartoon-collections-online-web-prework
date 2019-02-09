3f296fc3f7985469f4e1c62bd1f9c5a5fde7e901
Tracy Ngo <tngo1206@gmail.com>
2019-02-09 23:26:38 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,9 +17,7 @@ end
 def find_the_cheese(string_array) #code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
- return if string_array.include?("chedder") || string_array.include?("gouda") || string_array.include?("camembert")
-     
-  else
-    return nil
+  string_array.each do |cheese?|
+    cheese_types
   end
 end
